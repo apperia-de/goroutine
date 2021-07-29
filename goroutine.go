fe76cc9d424c688a8e7464132ce1d7e580cda136
Stephan Knauer <sknr@users.noreply.github.com>
2021-07-29 16:55:58 +0200

    Add tests + refactoring

--- goroutine.go
+++ goroutine.go
@@ -6,6 +6,9 @@ import (
 	"strings"
 )
 
+// The RecoverFunc type defines the signature of a recover function within a goroutine.
+type RecoverFunc func(v interface{})
+
 // The default recover function which will be used by the Go func for each goroutine.
 // Can be easily overridden with SetDefaultRecoverFunc in an init function in order to change the default behavior.
 var defaultRecoverFunc = func(v interface{}) {
@@ -14,44 +17,6 @@ var defaultRecoverFunc = func(v interface{}) {
 		return
 	}
 	fmt.Printf("Goroutine panic recovered: %v\n", v)
-}
-
-// The RecoverFunc type defines the signature of a recover function within a goroutine.
-type RecoverFunc func(v interface{})
-
-type goroutine struct {
-	goFn  interface{} // The goFn function which will be called in a goroutine.
-	recFn RecoverFunc // The recFn function which will be called if a panic has been recovered with that goroutine.
-}
-
-// The Go method starts a new goroutine, which is panic safe. A possible panic call will be gracefully recovered by the recover function g.recFn.
-func (g *goroutine) Go(params ...interface{}) {
-	go func() {
-		defer func() {
-			if r := recover(); r != nil && g.recFn != nil {
-				g.recFn(r)
-			}
-		}()
-		fnVal := reflect.ValueOf(g.goFn)
-		// Check input length
-		if lp, lf := len(params), fnVal.Type().NumIn(); lp != lf {
-			panic(fmt.Sprintf("The number of params (%d) does not match required function params (%d). Function signature: %s", lp, lf, signature(g.goFn)))
-		}
-		// Convert the input params
-		in := make([]reflect.Value, len(params))
-		for i, param := range params {
-			in[i] = reflect.ValueOf(param)
-		}
-		// Call the function
-		fnVal.Call(in)
-	}()
-}
-
-// WithRecoverFunc overrides the default recover function with fn.
-// Note: If you pass nil as a RecoverFunc, the panic will be silently recovered.
-func (g *goroutine) WithRecoverFunc(fn RecoverFunc) *goroutine {
-	g.recFn = fn
-	return g
 }
 
 // Goroutine creates a new panic safe goroutine, with the defaultRecoverFunc as recover function.
@@ -90,10 +55,52 @@ func Go(fn interface{}, params ...interface{}) {
 	Goroutine(fn).Go(params...)
 }
 
-// SetDefaultRecoverFunc can be used to override the defaultRecoverFunc which is used by Go and Goroutine if the input param is not a GoRoutiner.
-// Note: Calling panic() within the recover function fn, will cause the app to crash if a panic within the goroutine arise.
-func SetDefaultRecoverFunc(fn func(v interface{})) {
+// SetDefaultRecoverFunc can be used to override the defaultRecoverFunc which is used by Go and Goroutine functions.
+// Note: Calling panic() within the recover function fn, will cause the application to crash if a panic within the goroutine arise.
+//       If you pass nil as a RecoverFunc, the panic will be silently recovered.
+func SetDefaultRecoverFunc(fn RecoverFunc) {
 	defaultRecoverFunc = fn
+}
+
+// GetDefaultRecoverFunc returns the current default recover function for goroutines used by the Go and Goroutine functions.
+func GetDefaultRecoverFunc() RecoverFunc {
+	return defaultRecoverFunc
+}
+
+type goroutine struct {
+	goFn  interface{} // The goFn function which will be called in a goroutine.
+	recFn RecoverFunc // The recFn function which will be called if a panic has been recovered with that goroutine.
+}
+
+// The Go method starts a new goroutine, which is panic safe. A possible panic call will be gracefully recovered by the recover function g.recFn.
+func (g *goroutine) Go(params ...interface{}) {
+	go func() {
+		defer func() {
+			if r := recover(); r != nil && g.recFn != nil {
+				g.recFn(r)
+			}
+		}()
+		fnVal := reflect.ValueOf(g.goFn)
+		// Check input length
+		if lp, lf := len(params), fnVal.Type().NumIn(); lp != lf {
+			panic(fmt.Sprintf("Function signature: %s | The number of params (%d) does not match required function params (%d)\n", signature(g.goFn), lp, lf))
+		}
+		// Convert the input params
+		in := make([]reflect.Value, len(params))
+		for i, param := range params {
+			in[i] = reflect.ValueOf(param)
+		}
+		// Call the function
+		fnVal.Call(in)
+	}()
+}
+
+// WithRecoverFunc overrides the default recover function with fn.
+// Note: Calling panic() within the recover function fn, will cause the application to crash if a panic within the goroutine arise.
+//       If you pass nil as a RecoverFunc, the panic will be silently recovered.
+func (g *goroutine) WithRecoverFunc(fn RecoverFunc) *goroutine {
+	g.recFn = fn
+	return g
 }
 
 // signature returns the signature string of a given function.
--- goroutine_test.go
+++ goroutine_test.go
@@ -1,10 +1,98 @@
 package goroutine
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
+
+func TestGoroutine(t *testing.T) {
+	resultChan := make(chan string)
+	// Given some example functions
+	fn0 := func() {
+		resultChan <- "Hallo Welt"
+	}
+	fn1 := func(name string) {
+		resultChan <- fmt.Sprintf("Hallo %s", name)
+	}
+	fn2 := func(a, b int) {
+		resultChan <- fmt.Sprintf("%d / %d = %d", a, b, a/b)
+	}
+	recFn1 := func(v interface{}) {
+		resultChan <- fmt.Sprintf("%v", v)
+	}
+
+	tests := []struct {
+		name   string
+		fn     interface{}
+		params []interface{}
+		want   string
+	}{
+		{"Goroutine with a zero param function", fn0, []interface{}{}, "Hallo Welt"},
+		{"Goroutine with a one param function", fn1, []interface{}{"GoWorld"}, "Hallo GoWorld"},
+		{"Goroutine with a two param function", fn2, []interface{}{42, 2}, "42 / 2 = 21"},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			Goroutine(test.fn).Go(test.params...)
+			got := <-resultChan
+			assertOutput(t, got, test.want)
+		})
+	}
+
+	t.Run("Goroutine with a two param function and a custom recover function which recovered from a panic", func(t *testing.T) {
+		Goroutine(fn2).WithRecoverFunc(recFn1).Go(2, 0)
+		got := <-resultChan
+		want := "runtime error: integer divide by zero"
+		assertOutput(t, got, want)
+	})
+}
 
 func TestGo(t *testing.T) {
+	resultChan := make(chan string)
+	// Given some example functions
+	fn0 := func() {
+		resultChan <- "Hallo Welt"
+	}
+	fn1 := func(name string) {
+		resultChan <- fmt.Sprintf("Hallo %s", name)
+	}
+	fn2 := func(a, b int) {
+		resultChan <- fmt.Sprintf("%d / %d = %d", a, b, a/b)
+	}
 
+	tests := []struct {
+		name   string
+		fn     interface{}
+		params []interface{}
+		want   string
+	}{
+		{"Goroutine with a zero param function", fn0, []interface{}{}, "Hallo Welt"},
+		{"Goroutine with a one param function", fn1, []interface{}{"GoWorld"}, "Hallo GoWorld"},
+		{"Goroutine with a two param function", fn2, []interface{}{10, 2}, "10 / 2 = 5"},
+	}
 
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			Go(test.fn, test.params...)
+			got := <-resultChan
+			assertOutput(t, got, test.want)
+		})
+	}
 
+	t.Run("Goroutine with a two param function which recovered from a panic", func(t *testing.T) {
 
+		SetDefaultRecoverFunc(func(v interface{}) { resultChan <- fmt.Sprintf("%v", v) })
+		Go(fn2, 2, 0)
+		got := <-resultChan
+		want := "runtime error: integer divide by zero"
+		assertOutput(t, got, want)
+	})
+}
+
+func assertOutput(t *testing.T, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q, want %q", got, want)
+	}
 }
